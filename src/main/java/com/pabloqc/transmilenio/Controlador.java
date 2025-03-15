92fe09fa7c4b86f238331deea4c74cb3581a82a1
Pablo Enrique Quintero Callamand <pabloenriquequintero@gmail.com>
2025-03-14 19:12:32 -0500

    v1
    codigo reescrito

--- src/main/java/com/pabloqc/transmilenio/Controlador.java
+++ src/main/java/com/pabloqc/transmilenio/Controlador.java
@@ -5,45 +5,103 @@
 
 package com.pabloqc.transmilenio;
 
+import java.text.NumberFormat;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controlador {
     @FXML
-    private Label weekly;
+    private TextField costTravel;
+
     @FXML
-    private Label monthly;
+    private TextField costFood;
+
     @FXML
-    private TextField cost;
-    @FXML
-    private TextField days;
+    private TextField costExtra;
+
     @FXML
     private TextField doubleDays;
 
-    public Controlador() {
-    }
+    @FXML
+    private TextField classDays;
 
-    public String getCost() {
-        return this.cost.getText();
+    @FXML
+    private Label transportCost;
+
+    @FXML
+    private Label foodCost;
+
+    @FXML
+    private Label weeklyCosts;
+
+    @FXML
+    private Label monthlyCosts;
+
+    @FXML
+    public void initialize() {
+        makeNumeric(costTravel);
+        makeNumeric(costFood);
+        makeNumeric(costExtra);
+        makeNumeric(doubleDays);
+        makeNumeric(classDays);
+
+        this.weeklyCosts.setText("");
+        this.foodCost.setText("");
+        this.monthlyCosts.setText("");
+        this.transportCost.setText("");
     }
 
     public String getDays() {
-        return this.days.getText();
+        return this.classDays.getText();
     }
 
     public String getDoubleDays() {
         return this.doubleDays.getText();
     }
 
-    public void calcular() {
-        int c = Integer.parseInt(this.getCost(), 10);
-        int d = Integer.parseInt(this.getDays(), 10);
-        int ex = Integer.parseInt(this.getDoubleDays(), 10);
-        c *= 2;
-        int costoSemana = c * d + c * ex;
-        this.weekly.setText(String.valueOf(costoSemana));
-        this.monthly.setText(String.valueOf(costoSemana * 4));
+    public String getCostTransport() {
+        return this.costTravel.getText();
+    }
 
+    public String getCostFood() {
+        return this.costFood.getText();
+    }
+
+    public String getCostExtras() {
+        return this.costExtra.getText();
+    }
+
+    //Solo valores numericos
+    private void makeNumeric(TextField textField) {
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                textField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+    }
+
+    @FXML
+    private void calcularCostos() {
+        int travel = Integer.parseInt(this.getCostTransport(), 10);
+        int food = Integer.parseInt(this.getCostFood(), 10);
+        int days = Integer.parseInt(this.getDays(), 10);
+        int doubleTrips = Integer.parseInt(this.getDoubleDays(), 10);
+        int extras = Integer.parseInt(this.getCostExtras(), 10);
+        
+        int costoTransporte = (travel * days + travel * doubleTrips)*2;
+        int costoAlimentacion = food * days;
+        int costoSemanal = costoTransporte + costoAlimentacion + extras;
+        int costoMensual = costoSemanal * 4;
+
+        //Formato
+        NumberFormat numberFormat = NumberFormat.getInstance();
+
+        this.weeklyCosts.setText("Gastos semanales totales: " + numberFormat.format(costoSemanal));
+        this.monthlyCosts.setText("Gastos mensuales totales: " + numberFormat.format(costoMensual));
+
+        this.foodCost.setText("Gastos semanales en comida: " + numberFormat.format(costoAlimentacion));
+        this.transportCost.setText("Gastos semanales en transporte: " + numberFormat.format(costoTransporte));
     }
 }
--- src/main/java/com/pabloqc/transmilenio/Costos.java
+++ src/main/java/com/pabloqc/transmilenio/Costos.java
@@ -6,22 +6,27 @@
 package com.pabloqc.transmilenio;
 
 import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Transmilenio extends Application {
-    public Transmilenio() {
+public class Costos extends Application {
+    public Costos() {
     }
 
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Transmilenio.class.getResource("Pantalla.fxml"));
-        Scene scene = new Scene((Parent)fxmlLoader.load(), 371.0, 330.0);
-        stage.setTitle("Calculador de pasaje");
+        try {
+        FXMLLoader fxmlLoader = new FXMLLoader(Costos.class.getResource("/com/pabloqc/transmilenio/Pantalla.fxml"));
+        Scene scene = new Scene((Parent)fxmlLoader.load(), 652, 400);
+        stage.setTitle("Calculadora de costos");
         stage.setScene(scene);
         stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
